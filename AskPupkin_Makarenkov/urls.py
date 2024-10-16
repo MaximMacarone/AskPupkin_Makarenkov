a2963e9919aa32634fc1f130c0ebc2e9545ed4c5
maximmacarone <maximmacarone@gmail.com>
2024-10-16 14:28:06 +0300

    impl: question.py, questionCard.html, index.html

--- AskPupkin_Makarenkov/urls.py
+++ AskPupkin_Makarenkov/urls.py
@@ -20,5 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.base, name='base'),
+    path('', views.index, name='index'),
 ]
--- AskPupkin_Makarenkov/views.py
+++ AskPupkin_Makarenkov/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse, request
 from django.shortcuts import render
 
+from AskPupkin_Makarenkov.question import Question
+
+
 def base(request):
     return render(request, 'static/base.html')
+
+def index(request):
+    questions = Question.get_mock_questions()
+    return render(request, 'static/index.html', {'questionCards': questions})
